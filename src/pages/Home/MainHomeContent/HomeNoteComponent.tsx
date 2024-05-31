a1da2f1050f441aa5d4e1b2460dce82df06bcf04
sesp13 <sespinosarte99@gmail.com>
2024-05-30 23:36:57 -0500

    use service to get available notes

--- src/pages/Home/MainHomeContent/HomeNoteComponent.tsx
+++ src/pages/Home/MainHomeContent/HomeNoteComponent.tsx
@@ -1,17 +1,19 @@
-export const HomeNoteComponent = () => {
+interface HomeNoteProps {
+  title: string;
+  content: string;
+  noteDate: string;
+}
+
+export const HomeNoteComponent = (props: HomeNoteProps) => {
+  const { title, content, noteDate } = props;
   return (
     <div className="border border-secondary mt-5 p-5">
-      <h4 className="text-main font-bold text-3xl">Sample Note</h4>
+      <h4 className="text-main font-bold text-3xl">{title}</h4>
       <p className="mt-2">
-        Category: <span className="italic text-main">School</span>- Date:{' '}
-        <span className="italic text-main"> 12/12/2021</span>
+        Category: <span className="italic text-main">School</span>- Date:
+        <span className="italic text-main"> {noteDate}</span>
       </p>
-      <p className="mt-3">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo fugiat
-        adipisci maiores. Eligendi, fugit. Sed officia sunt quae, necessitatibus
-        error dicta maiores incidunt enim esse, fugiat, totam debitis aut
-        possimus?
-      </p>
+      <p className="mt-3">{content}</p>
     </div>
   );
 };
--- src/pages/Home/MainHomeContent/MainHomeContent.tsx
+++ src/pages/Home/MainHomeContent/MainHomeContent.tsx
@@ -1,12 +1,33 @@
-import { HomeNoteComponent } from "./HomeNoteComponent";
+import { useEffect, useState } from 'react';
+
+import { HomeNoteComponent } from './HomeNoteComponent';
+import { INote } from '../../../models';
+import { useNoteService } from '../../../hooks';
 
 export const MainHomeContent = () => {
+  const [notes, setNotes] = useState<INote[]>([]);
+  const { getAllNotes: getAllNotesApi } = useNoteService();
+
+  useEffect(() => {
+    getAllNotes();
+  }, []);
+
+  const getAllNotes = async () => {
+    const res = await getAllNotesApi();
+    setNotes(res);
+  };
+
   return (
     <section className="w-2/3">
       <h3 className="text-4xl text-main font-bold">Last Notes</h3>
-      <HomeNoteComponent />
-      <HomeNoteComponent />
-      <HomeNoteComponent />
+      {notes.map((note) => (
+        <HomeNoteComponent
+          key={note.id}
+          title={note.name}
+          content={note.content}
+          noteDate={new Date().toISOString()}
+        />
+      ))}
     </section>
   );
 };
